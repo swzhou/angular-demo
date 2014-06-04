496ba40fae4167e38a5de8c63028ef156fb349ad
swzhou <swing.zhou@hotmail.com>
2014-06-04 23:10:15 +0800

    [sw] dialog

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,26 @@
 'use strict';
-
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('myApp.controllers', ['ui.bootstrap'])
+    .controller('MyCtrl1', ['$scope', '$modal', function ($scope, $modal) {
+        $scope.open = function () {
+            $modal.open({
+                templateUrl: 'partials/dialog.html',
+                controller: CreateSthController,
+                backdrop: true
+            }).result.then(function () {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+            });
+        };
+    }])
+    .controller('MyCtrl2', ['$scope', function ($scope) {}]);
 
-  }]);
+    var CreateSthController =  function ($scope, $modalInstance) {
+        $scope.ok = function () {
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
