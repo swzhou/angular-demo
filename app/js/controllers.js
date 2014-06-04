e1a116845cb6197947dbd6a441050e704822b2e7
swzhou <swing.zhou@hotmail.com>
2014-06-04 23:20:05 +0800

    [sw] dialog

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,21 +3,31 @@
 
 angular.module('myApp.controllers', ['ui.bootstrap'])
     .controller('MyCtrl1', ['$scope', '$modal', function ($scope, $modal) {
+        $scope.items = ['item1', 'item2', 'item3'];
         $scope.open = function () {
             $modal.open({
                 templateUrl: 'partials/dialog.html',
                 controller: CreateSthController,
-                backdrop: true
-            }).result.then(function () {
-
+                backdrop: true,
+                resolve: {
+                    items: function() {
+                        return $scope.items;
+                    }
+                }
+            }).result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
             });
         };
     }])
     .controller('MyCtrl2', ['$scope', function ($scope) {}]);
 
-    var CreateSthController =  function ($scope, $modalInstance) {
+    var CreateSthController =  function ($scope, $modalInstance, items) {
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
         $scope.ok = function () {
-            $modalInstance.close();
+            $modalInstance.close($scope.selected.item);
         };
 
         $scope.cancel = function () {
